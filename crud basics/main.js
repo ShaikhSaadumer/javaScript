01b475eda1115a63474b9d265613ff87c1b37461
ShaikhSaadumer <m.saad12378688@gmail.com>
2022-07-01 14:15:53 -0700

    Media App
    Crud Basics

---
+++ crud basics/main.js
@@ -0,0 +1,60 @@
+let from = document.getElementById("form");
+let input =document.getElementById("input");
+let msg =document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+from.addEventListener("submit", (e) =>{
+  e.preventDefault();
+  console.log("button clicked");
+formValidation();  
+
+}) 
+
+let formValidation = () =>{
+  if(input.value === "")
+  {
+    msg.innerHTML = "Post can not be blank" ;
+    console.log("Failure")
+
+  }
+  else
+  {
+    
+    console.log("Success");
+    msg.innerHTML = "" 
+    acceptData();
+  }
+}
+
+let data = {};
+
+let acceptData = () => {
+
+  data ["text"] = input.value;
+  console.log(data);
+  createPost ( ) ;
+};
+
+let createPost = () => {
+
+  posts.innerHTML  += `
+   <div>
+  <P>${data.text}</P>
+  <div class="options">
+    <i onclick= "editPost(this)" class="fa-solid fa-pen-to-square"></i>
+    <i onclick= "deletePost(this) " class="fa-solid fa-trash"></i>
+  </div>
+</div> ` ;
+input.value ="";
+};
+
+let deletePost = (e) => {
+e.parentElement.parentElement.remove();
+
+};
+
+let editPost = (e) => {
+  input.value = e.parentElement.previousElementSibling.innerHTML;
+  e.parentElement.parentElement.remove();
+
+}
